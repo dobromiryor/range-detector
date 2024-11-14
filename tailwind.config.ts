2077c98ea682b71e49d450366af4e729ed3edaa3
Dobromir Yordanov <32433021+dobromiryor@users.noreply.github.com>
2024-11-14 15:59:49 +0200

    feat: 💄 updated default font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "tailwindcss";
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Geologica", "sans-serif"],
+			},
+		},
 	},
 	plugins: [],
 } satisfies Config;
