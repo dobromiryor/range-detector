12ce78be76804794133fbd8df3e16a682ef071e5
Dobromir Yordanov <32433021+dobromiryor@users.noreply.github.com>
2024-11-14 16:03:08 +0200

    refactor: 🥅 updated errors in parse file function

--- src/utils/parse-file.ts
+++ src/utils/parse-file.ts
@@ -2,13 +2,22 @@ import { partnerSchema, type Partner } from "@/schemas/partner.schema";
 
 export const parsePartnersFromFile = (fileContent: string): Partner[] => {
 	const lines = fileContent.split(/\r?\n/).filter((line) => line.trim() !== "");
-	const parsedLines = lines.map((line) => JSON.parse(line));
+
+	let parsedLines: object[] | null = null;
+
+	try {
+		parsedLines = lines.map((line) => JSON.parse(line));
+	} catch (_) {
+		throw new Error("Invalid file type or content.");
+	}
+
+	if (parsedLines.length === 0) throw new Error("File is empty.");
 
 	const { success, data: parsedPartners } = partnerSchema
 		.array()
 		.safeParse(parsedLines);
 
-	if (!success) throw new Error("Invalid file content.");
+	if (!success) throw new Error("Invalid content schema.");
 
 	return parsedPartners;
 };
